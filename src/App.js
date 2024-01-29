1b787c9858c1cac238f04304a816a66dd966b008
Sakeef Hassan <91638600+sakeefh@users.noreply.github.com>
2024-01-29 17:32:04 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HelloWorld from './components/HelloWorld'; // Import your component
+
+function App() {
+  return (
+    <div>
+      <HelloWorld /> {/* Render your component */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/HelloWorld.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Define a functional component named HelloWorld
+function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is my first React component.</p>
+    </div>
+  );
+}
+
+export default HelloWorld;
