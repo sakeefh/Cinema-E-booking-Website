fa91469dda5ffed590558571675466fd9d3127de
Sakeef Hassan <91638600+sakeefh@users.noreply.github.com>
2024-04-15 14:26:35 -0400

    Delete cinema directory

--- cinema/theatre/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Movie, User
-
-admin.site.register(Movie)
-# Register your models here.
-admin.site.register(User)
--- cinema/theatre/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('register/', register_user, name='register_user'),
-    path('users/',user_list,name='user_list'),
-    path('',movies,name="movies"),
-    path('movies/<int:movie_id>/', movie_details, name='movie-details'),
-]
